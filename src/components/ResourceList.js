8da0ca1d248bfec2d1f8ec10c977d8bfa235eaaa
bradleysimmons <bradleysimmons9@gmail.com>
2019-06-26 22:14:41 -0500

    writes it out with two lifecycle methods

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -2,14 +2,25 @@ import React from 'react';
 import axios from 'axios';
 
 class ResourceList extends React.Component {
+  state = { resources: [] }
 
-  componentDidMount() {
-    axios.get(`https://jsonplaceholder.typeicode.com/${this.props.resource}`)
+  async componentDidMount() {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+
+    this.setState({ resources: response.data })
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource != this.props.resource) {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+
+      this.setState({ resources: response.data })
+    }
   }
 
   render() {
     return (
-      <div>{this.props.resource}</div>
+      <div>{this.state.resources.length}</div>
     )
   }
 }
