11aa127718c2f16c0cb39ac3ec3728573d023d52
bradleysimmons <bradleysimmons9@gmail.com>
2019-06-26 22:29:21 -0500

    refactors to functional component with useState and useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,28 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = { resources: [] }
+const ResourceList = ({ resource }) => {
 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+  const [resources, setResources] = useState([]);
 
-    this.setState({ resources: response.data })
+  const fetchResource = async () => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+
+    setResources(response.data)
   }
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource != this.props.resource) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-
-      this.setState({ resources: response.data })
-    }
-  }
-
-  render() {
-    return (
-      <div>{this.state.resources.length}</div>
-    )
-  }
+  useEffect(() => {
+    fetchResource(resource)
+  }, [])
+  
+  return <div>{resources.length}</div>
+  
 }
 
 export default ResourceList;
