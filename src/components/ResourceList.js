09fd591fef3bc8b0e7ab9aba294d60d8666e321b
bradleysimmons <bradleysimmons9@gmail.com>
2019-06-26 21:56:56 -0500

    installs axios; api call to jsonplaceholder

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typeicode.com/${this.props.resource}`)
+  }
+
   render() {
     return (
       <div>{this.props.resource}</div>
